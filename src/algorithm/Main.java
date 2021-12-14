246e9e2e519a9cd7708c51bef2f743c37726b471
Julia Song <julja.song@gmail.com>
2021-12-14 18:18:15 +0900

    test

--- src/algorithm/Main.java
+++ src/algorithm/Main.java
@@ -1,18 +1,10 @@
 package algorithm;
 
-import java.util.Scanner;
-
 public class Main {
-  
+
   public static void main(String[] args) throws Exception {
-    
-      Scanner sc = new Scanner(System.in);
-      StringBuilder sb = new StringBuilder();
-      
-     double r = sc.nextInt();
-     
-     System.out.printf("%f\n", Math.PI * r * r);
-     System.out.printf("%f\n", 2 * r * r);
+
+      System.out.println("connect test");
       
   }
 }
